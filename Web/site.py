c18e4f48951e7f4f7a4793370af7f89774fde038
vitasgit <vibrantfeynman@gmail.com>
2026-02-17 19:41:31 +0500

    шаблон для отправки данных orangepi

--- Web/site.py
+++ Web/site.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
+    #if request.method == "POST":
+        # cmd = request.POST.get('submit')
+        # cmd = request.form['submit']
+        # как передать команду orangepi?
+    
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run()
