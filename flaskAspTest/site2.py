1b7cfc007ef55d0fd13d5c65ad6f629cabfd8744
vitasgit <vibrantfeynman@gmail.com>
2026-02-24 19:34:47 +0500

    2 задачи

---
+++ flaskAspTest/site2.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+from flask_apscheduler import APScheduler
+
+app = Flask(__name__)
+count = 0
+@app.route("/")
+def index():
+    return render_template('index.html', count=count)
+
+
+# экземпляр класса APScheduler
+sched = APScheduler()
+@sched.task('interval', id='do_job_1', seconds=5, misfire_grace_time=900)
+def job1():
+    global count  # нужен, когда хочешь изменить внешнюю переменную
+    count += 1
+
+@sched.task('interval', id='do_job_2', seconds=10, misfire_grace_time=900)
+def job2():
+    global count  # нужен, когда хочешь изменить внешнюю переменную
+    count += 100
+
+
+if __name__ == '__main__':
+    #app = Flask(__name__)
+
+    # SCHEDULER_API_ENABLED: bool, значение по умолчанию: False - включает/отключает API планировщика;
+    sched.api_enabled = True
+    sched.init_app(app)
+    sched.start()
+    print(sched.get_jobs())  # список задач
+    app.run()
