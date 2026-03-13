45e2597c3a7bc712dccb2863a0c618a46809d1fe
vitasgit <vibrantfeynman@gmail.com>
2026-03-13 13:42:27 +0500

    включение-выключение диода с помощью sched

--- flaskAspTest/sched.py
+++ flaskAspTest/sched.py
@@ -1,28 +1,21 @@
 from flask import Flask, render_template, request
-from RF24 import RF24, RF24_PA_MAX, RF24_250KBPS, RF24_DRIVER
-from flask import Flask, render_template
+from RF24 import RF24, RF24_PA_LOW, RF24_250KBPS, RF24_DRIVER
 from flask_apscheduler import APScheduler
 import time
 
 
 app = Flask(__name__)
-
-
-# экземпляр класса APScheduler
 sched = APScheduler()
+
 @sched.task('interval', id='do_job_1', seconds=5, misfire_grace_time=900)
 def job1():
     send_cmd("1")
 
+@sched.task('interval', id='do_job_2', seconds=10, misfire_grace_time=900)
+def job2():
+    send_cmd("0")
 
-"""
-Не всегда передается сигнал. 
-Возможно причины в следующем:
-1) плохой pipe адрес ()
-2) плохой формат передачи данных (передаю всего 1 бит)  !!! скроее всего в этом проблема
-3) нестабильное питание, нужно добавить конденсатор в схему
-4) помехи на канале (изначально выбирался без помех, маловероятно что появились)
-"""
+
 def send_cmd(cmd):
     if cmd == "1":
         data = b"\x01"
@@ -56,21 +49,26 @@ def index():
 if __name__ == '__main__':
     CE_PIN = 73  # pin 7 на плате
     CSN_PIN = 11  # для spidev1.1
-    pipe1_addr = b"\x01\x02\x03\x04\x05"
+    pipe1_addr = b"1Node"
     radio = RF24(CE_PIN, CSN_PIN)
 
     if not radio.begin():
         print("радиомодуль не подключен")
         #exit()
         
-    radio.setPALevel(RF24_PA_MAX)
+    radio.setPALevel(RF24_PA_LOW)        # мощность передатчика (low = -12 dBm)
+    radio.setDataRate(RF24_250KBPS)      # Скорость передачи данных, чем меньше - тем дальше (скорость приема и передачи должна быть одинаковая)
     radio.setAutoAck(1);                 # режим подтверждения приёма, 1 вкл 0 выкл
     radio.setRetries(0, 15);             # (время между попыткой достучаться, число попыток) 15 - максимальное
     radio.setPayloadSize(1);             # пакет данных размером 1 байт (бубу передавать 1 или 0)
-    radio.setDataRate(RF24_250KBPS)      # Скорость передачи данных, чем меньше - тем дальше (скорость приема и передачи должна быть одинаковая)
     radio.setChannel(76)                 # выбираем канал передачи данных с самыми низкими помехами
     radio.powerUp();                     # режим передачи (повышенного потребления), powerDown - режим ожидания
     radio.openWritingPipe(pipe1_addr);   # открыть канал на отправку
     radio.stopListening()                # режим передачи
+
+    sched.api_enabled = True
+    sched.init_app(app)
+    sched.start()
+    print(sched.get_jobs())  # список задач
 
     app.run(host='0.0.0.0', port=5000, debug=False)
