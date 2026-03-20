5ef29ef7dba9e6b08be9bb7ad6a6eed5df8d4b9d
vitasgit <vibrantfeynman@gmail.com>
2026-03-20 19:15:24 +0500

    mosfet транзистор управление вентилятором

--- ard/mosfet-air/mosfet-air/mosfet-air.ino
+++ ard/mosfet-air/mosfet-air/mosfet-air.ino
@@ -4,26 +4,24 @@ void setup()
 {
   pinMode(TRIG, OUTPUT);      // Устанавливаем вывод как выход
   digitalWrite(TRIG, LOW);    // Устанавливаем вывод в LOW
+
+  // меняем частоту ШИМ с 490 Гц на 31250 Гц (31 кГц)
+  TCCR2B = (TCCR2B & 0b11111000) | 0x01;
 }
  
 void loop()
 {
   // digitalWrite(TRIG, HIGH);   // Включаем диод 
-  // delay(1000);                // Пауза 2 с
+  // delay(2000);                // Пауза 2 с
   // digitalWrite(TRIG, LOW);    // Выключаем диод 
-  // delay(1000);                // Пауза
+  // delay(2000);                // Пауза
  
 // включить на x% и выключить
   analogWrite(TRIG, 64);
-  delay(1000);
-  digitalWrite(TRIG, LOW);
-  delay(1000);
-  analogWrite(TRIG, 128);
-  delay(1000);
-  digitalWrite(TRIG, LOW);
-  delay(1000);
+  delay(5000);
+//  analogWrite(TRIG, 128);
+//   delay(1000);
   analogWrite(TRIG, 255);
-  delay(1000);
-  digitalWrite(TRIG, LOW);
-  delay(1000);
+  delay(5000);
+  //delay(1000);
 }
